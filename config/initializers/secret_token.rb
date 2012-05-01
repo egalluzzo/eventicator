67849bc698441ebb0555aaadb792850aa2a767be
Eric Galluzzo <egalluzzo@gmail.com>
2012-04-30 22:57:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventicator::Application.config.secret_token = '8589222d0987bbf7a2f9e7e7ed0dc7d5868a66a023688d94430339bce76dac80e154835b0874b5056bc79ca847f7fccb441151b5852b4e4044a051f83cf55f48'
