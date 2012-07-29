31043b0ff845dba7901897d2d759c3b4ed533bb5
Eric Galluzzo <egalluzzo@gmail.com>
2012-07-29 12:11:42 -0400

    Working on creating an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,18 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params[:event])
+    if @event.save
+      flash[:success] = "Created event #{event.name}"
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
 end
--- spec/requests/event_pages_spec.rb
+++ spec/requests/event_pages_spec.rb
@@ -9,7 +9,50 @@ describe "Event pages" do
     before { visit event_path(event) }
 
     it { should have_selector('h1',    text: event.name) }
-    it { should have_selector('title', text: "Event | #{event.name}") }
+    it { should have_selector('title', text: "#{event.name}") }
+  end
+
+  describe "creating a new event" do
+    before { visit new_event_path }
+
+    it { should have_selector('h1',    text: 'Create Event') }
+    it { should have_selector('title', text: 'Create Event') }
+
+    describe "with invalid information" do
+      it "should not create an event" do
+        expect { click_button 'Create' }.not_to change(Event, :count)
+      end
+
+      describe "after submission" do
+        before { click_button 'Create' }
+
+        it { should have_selector('title', text: 'Create Event') }
+        it { should have_content("can't be") }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",        with: "My Fabulous Event"
+        fill_in "Location",    with: "Duke Energy Convention Center\n525 Elm Street\nCincinnati, OH 45202"
+        fill_in "Description", with: "A description for my fabulous event"
+        fill_in "Start date",  with: "2012-08-23"
+        fill_in "End date",    with: "2012-08-24"
+      end
+
+      it "should create an event" do
+        expect { click_button 'Create' }.to change(Event, :count).by(1)
+      end
+
+      describe "after creating an event" do
+        before { click_button 'Create' }
+        let(:event) { Event.find_by_name('My Fabulous Event') }
+
+        # We should end up on the "show event" page.
+        it { should have_selector('title',                   text: event.name) }
+        it { should have_selector('div.alert.alert-success', text: "Created event #{event.name}") }
+      end
+    end
   end
 end
 
